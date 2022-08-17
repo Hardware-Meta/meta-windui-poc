bcaa85162a96bfbe3d1028c3ee8b5c6629ee4740
johnweland <john.weland@gmail.com>
2022-08-17 00:10:20 +0000

    Deploying to gh-pages from @ Hardware-Meta/meta-windui-poc@ec419bb9d4bc6b7e2c36d586ab965328d907a25e 🚀

---
+++ _next/static/6XhXGQg2Q6PiYy762IQja/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-42fae28c0308156b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/post":["static/chunks/pages/post-56480f34890c18ae.js"],sortedPages:["/","/_app","/_error","/post"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/76zi-SpOSCvxd_ctISqSi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-42fae28c0308156b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/post":["static/chunks/pages/post-a449e9b168ea79d7.js"],sortedPages:["/","/_app","/_error","/post"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
