1e1b6fd70580df6e56c75e9e46659a43be0dfb43
johnweland <john.weland@gmail.com>
2022-10-10 14:02:58 +0000

    Deploying to gh-pages from @ Hardware-Meta/meta-windui-poc@7513e6691f94b789517369b1edbb6d4a4ed7c1fb 🚀

---
+++ _next/static/2b_EMKusLxaot7JqEp2uF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-42fae28c0308156b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/post":["static/chunks/pages/post-8e5bd922e11ee6b0.js"],sortedPages:["/","/_app","/_error","/post"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
